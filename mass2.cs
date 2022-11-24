2716671ae304dd0ffb77f7ab2e95b83a0a0830ff
SAKURAI20341 <106017636+SAKURAI20341@users.noreply.github.com>
2022-11-24 21:44:19 +0400

    arrays
    arrays

---
+++ mass2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int h = 0;
+            string text = Console.ReadLine();
+            string text1 = Console.ReadLine();
+            for (int i = 0; i < text.Length; i++)
+            {
+
+                if (text[i] == text1[i])
+                {
+                    h++; ;
+                }
+            }
+            Console.WriteLine($" совпадений {h} элементов:");
+        }
+    }
+}
---
+++ mass5.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string text = Console.ReadLine();
+            int[] text1 = text.Select(number => Convert.ToInt32(Char.GetNumericValue(number))).ToArray();
+            int max = text1[0];
+            int min = text1[0];
+
+            for (int i = 0; i < text1.Length; i++)
+            {
+                if (text1[i] < min)
+                {
+                    min = (text1[i]);
+                }
+                if (text1[i] > max)
+                {
+                    max = text1[i];
+                }
+
+            }
+            Array.Reverse(text1);
+            foreach (int n in text1)
+            {
+                Console.Write(n);
+            }
+
+
+
+            Console.Write($"{max}{min}");
+        }
+    }
+}
