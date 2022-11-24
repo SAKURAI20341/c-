8396e3ce579888e45ab6b8166669580cb9ff93c0
SAKURAI <SAKURAI@DESKTOP-23KUPUM>
2022-11-24 21:00:05 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+            int a = 0;
+            int b = 5;
+            for(int i=0;i<n;i++)
+            {
+                a=a+ 1;
+                b = b + 5;
+                if(a==13)
+                {
+                    a = 1;
+                }
+                if(b==60)
+                {
+                    b = 0;
+                }
+            }
+            if (b == 5 || b == 0)
+            {
+                Console.WriteLine($"{a}:0{b}");
+            }
+            else
+            { 
+                Console.WriteLine($"{a}:{b}"); 
+            }
+        }
+    }
+}
