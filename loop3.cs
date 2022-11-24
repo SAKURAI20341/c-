45134fb41fb21216db08c77a695d9d1b36f3e5cc
SAKURAI20341 <106017636+SAKURAI20341@users.noreply.github.com>
2022-11-24 21:48:15 +0400

    Add files via upload

---
+++ loop3.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad2
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = x;
+            for (int i = 0, z = 1; i < x; i++, z += 2)
+            {
+                Console.Write(new string(' ', y--));
+                Console.Write(new string('*', z));
+                Console.WriteLine();
+            }
+
+
+
+        }
+    }
+}
